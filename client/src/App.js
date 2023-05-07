c49adb24d1647c68c05fdfa75ef5c2c2aa17b360
Przemysław Nitoń <przemek.niton@gmail.com>
2023-05-07 16:21:40 +0200

    delete button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <List/>
     </div>
   );
 }
