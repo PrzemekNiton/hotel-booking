90c4dc7a4ef8c8ac3ac279e9929bc589c9da30f0
Przemysław Nitoń <przemek.niton@gmail.com>
2023-05-09 19:19:37 +0200

    add nodemon server

---
+++ server.js
@@ -0,0 +1,12 @@
+// instalacja node servera
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//instalacja nodemon ,który restartuje atumotatycznie dane node.js po zmianie w plikach projektu
+app.listen(port, () => console.log(`Node Server Started`));
+
+
+
