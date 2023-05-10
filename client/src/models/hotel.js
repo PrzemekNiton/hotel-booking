8e6087d0f683f74932be78c2062ec2f0540d5292
Aleksandra Bozenko <aleksandra.b.20018@gmail.com>
2023-05-10 19:52:48 +0200

    Add hotel schema

---
+++ client/src/models/hotel.js
@@ -0,0 +1,37 @@
+const mongoose = require("moongose");
+
+//funckja konstruktora, pozwala na definiowanie schemmatu dla kolekcji tabeli w bazie danych Mongo
+const hotelSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    maxGuests:{
+        type: Number,
+        required: true
+    },
+    country:{
+        type: Number,
+        required: true
+    },
+    pricePerNight:{
+        type: Number,
+        required: true
+    },
+    imageUrls: [],
+    currentBookings: [],
+    typeOfHotel:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true,
+})
+
+const hotelModel = mongoose.model('hotel', hotelSchema)
+
+module.exports = hotelModel
